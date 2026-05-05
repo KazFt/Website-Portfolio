513c610ff173a7332da64d2fc1b9c785fdf1297c
Kaz <emmanueljohnn31@gmail.com>
2026-05-06 00:58:22 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -36,7 +36,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // Fetch all projects
 async function fetchProjects() {
   try {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/projects/featured");
+    const response = await fetch("https://web-production-c197e.up.railway.app");
     const projects = await response.json();
     console.log(projects);
   } catch(e) {
